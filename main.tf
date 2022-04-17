bb32e6d84ffcf55d1551492ccc68a7c65de7232e
Harry <47417398+HarryS028@users.noreply.github.com>
2022-04-17 10:26:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  alias      = "backend"
   region     = "us-east-1"
   access_key = var.ACCESS_KEY
   secret_key = var.SECRET_KEY
@@ -10,8 +9,4 @@ module "nfid" {
   client_id     = var.CLIENT_ID
   client_secret = var.CLIENT_SECRET
   redirect_uri  = var.REDIRECT_URI
-
-  providers = {
-    aws = aws.backend
-  }
 }
