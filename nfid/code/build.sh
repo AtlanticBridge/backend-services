7f1b97c87e1b482717f40fbdc3e45f4acacdbce9
Harry Shuttleworth <hshuttleworth0@gmail.com>
2022-05-02 10:39:37 +0100

    Update build script

--- nfid/code/build.sh
+++ nfid/code/build.sh
@@ -6,12 +6,16 @@ echo 'Print here'
 echo ${PWD}
 echo ${BASEDIR}
 
-python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'
+python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'
+type -P python3 >/dev/null 2>&1 && echo Python 3 is installed
 
 pip3 install --target ${PWD}/${BASEDIR}/dependencies/python -r ${PWD}/${BASEDIR}/requirements.txt
+echo pip3 --version
 
 search_dir=${PWD}/${BASEDIR}/dependencies/python
 for entry in "$search_dir"/*
 do
   echo "$entry"
 done
+
+echo 'End build script'
