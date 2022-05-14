5b307938e3cf34817be792799a3538f2ffa8db8f
karthedew <karthedew@gmail.com>
2022-05-14 14:56:30 -0600

    removed mint_key module

--- main.tf
+++ main.tf
@@ -39,13 +39,13 @@ module "nfid" {
 }
 */
 
-module "mint_key" {
-  source = "./mint"
-  client_id     = var.CLIENT_ID
-  client_secret = var.CLIENT_SECRET
-  redirect_uri  = var.REDIRECT_URI
-  mint_private_key = var.MINT_PRIVATE_KEY
-  infura_url = var.INFURA_URL
-  nfid_contract_address = var.NFID_CONTRACT_ADDRESS
-  jwt_secret    = var.JWT_SECRET
-}
+# module "mint_key" {
+#   source = "./mint"
+#   client_id     = var.CLIENT_ID
+#   client_secret = var.CLIENT_SECRET
+#   redirect_uri  = var.REDIRECT_URI
+#   mint_private_key = var.MINT_PRIVATE_KEY
+#   infura_url = var.INFURA_URL
+#   nfid_contract_address = var.NFID_CONTRACT_ADDRESS
+#   jwt_secret    = var.JWT_SECRET
+# }
