5ebb9ccc2313040de9f35b565a8d989eb4504497
karthedew <karthedew@gmail.com>
2022-05-17 20:30:09 -0600

    changed main.tf to be exactly the same as what's in develop branch

--- main.tf
+++ main.tf
@@ -26,11 +26,19 @@ module "nfid" {
   client_id     = var.CLIENT_ID
   client_secret = var.CLIENT_SECRET
   redirect_uri  = var.REDIRECT_URI
-  mint_private_key = var.MINT_PRIVATE_KEY
-  infura_url = var.INFURA_URL
-  nfid_contract_address = var.NFID_CONTRACT_ADDRESS
   jwt_secret    = var.JWT_SECRET
 }
+
+# module "nfid" {
+#   source        = "./nfid"
+#   client_id     = var.CLIENT_ID
+#   client_secret = var.CLIENT_SECRET
+#   redirect_uri  = var.REDIRECT_URI
+#   mint_private_key = var.MINT_PRIVATE_KEY
+#   infura_url = var.INFURA_URL
+#   nfid_contract_address = var.NFID_CONTRACT_ADDRESS
+#   jwt_secret    = var.JWT_SECRET
+# }
 
 /* resource "<provider>_<resource_type>" "name" { 
   config options....
