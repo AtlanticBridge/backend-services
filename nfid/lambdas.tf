0e2af6945213226a91e130e758c8464ebd0533d9
karthedew <karthedew@gmail.com>
2022-05-17 20:25:18 -0600

    changed the NFID lambdas to what is in the develop branch

--- nfid/lambdas.tf
+++ nfid/lambdas.tf
@@ -100,6 +100,81 @@ resource "aws_lambda_function" "nfid_sign_in_lambda" {
   }
 }
 
+resource "aws_iam_role" "nfid_refresh_token_lambda_role" {
+  name = "nfid_refresh_token_lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "nfid_refresh_token_lambda_policy" {
+  name   = "nfid_refresh_token_lambda_policy"
+  role   = aws_iam_role.nfid_refresh_token_lambda_role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+              "${aws_dynamodb_table.nfid_users.arn}",
+              "arn:aws:logs:*:*:*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+data "archive_file" "nfid_refresh_token_lambda_archive" {
+  type        = "zip"
+  source_file = "${path.module}/code/nfid_sign_in.py"
+  output_path = "${path.module}/code/nfid_sign_in.zip"
+}
+
+resource "aws_lambda_function" "nfid_refresh_token_lambda" {
+  filename         = data.archive_file.nfid_refresh_token_lambda_archive.output_path
+  source_code_hash = data.archive_file.nfid_refresh_token_lambda_archive.output_base64sha256
+  publish          = true
+  function_name    = "nfid_refresh_token_lambda"
+  role             = aws_iam_role.nfid_refresh_token_lambda_role.arn
+  handler          = "nfid_refresh_token.lambda_handler"
+  runtime          = "python3.8"
+  timeout          = 15
+  layers           = [aws_lambda_layer_version.nfid_layer.arn]
+
+  environment {
+    variables = {
+      CLIENT_ID     = var.client_id
+      CLIENT_SECRET = var.client_secret
+      REDIRECT_URI  = var.redirect_uri
+      TABLE_NAME    = aws_dynamodb_table.nfid_users.id
+      PYTHONPATH    = "/opt"
+      JWT_SECRET    = var.jwt_secret
+    }
+  }
+}
+
 /*
   |========================|
   |    --- MINT KEY ---    |
