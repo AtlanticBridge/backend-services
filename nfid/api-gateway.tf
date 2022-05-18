aeddd25f281544ef4772edb35d94e2ab3c465a1a
karthedew <karthedew@gmail.com>
2022-05-17 20:49:29 -0600

    changed all equals signs to be 'equal'

--- nfid/api-gateway.tf
+++ nfid/api-gateway.tf
@@ -247,165 +247,165 @@ NEED FOR EACH METHOD:
   [4] aws_api_gateway_stage
 */
 
-# resource "aws_api_gateway_rest_api" "mint_api" {
-#   name        = "mint_api"
-#   description = "API for Mint Key Lambda Function"
-#   endpoint_configuration {
-#     types = ["REGIONAL"]
-#   }
+resource "aws_api_gateway_rest_api" "mint_api" {
+  name        = "mint_api"
+  description = "API for Mint Key Lambda Function"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+// ------------------------------------------------------------------------------
+
+resource "aws_api_getway_resource" "create_mint_key_resource" {
+  parent_id   = aws_api_gateway_rest_api.mint_api.root_resource_id
+  parth_part  = "mint_api"
+  rest_api_id = aws_api_gateway_rest_api.mint_api.id
+}
+
+# resource "aws_api_gateway_resource" "create_nfid_resource" {
+#   path_part   = "create_nfid"
+#   parent_id   = aws_api_gateway_rest_api.nfid_api.root_resource_id
+#   rest_api_id = aws_api_gateway_rest_api.nfid_api.id
 # }
 
-# // ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+#                       ===============================
+#                        --- API GATEWAY METHOD(s) ---
+#                       ===============================
 
-# resource "aws_api_getway_resource" "create_mint_key_resource" {
-#   parent_id = aws_api_gateway_rest_api.mint_api.root_resource_id
-#   parth_part = "mint_api"
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
-# }
+#                       !!!!!!!!!!!!!!
+#                       --- ITEM 1 ---
+#                       !!!!!!!!!!!!!!
+// MINT Post Method
+resource "aws_api_gateway_method" "mint_post_method" {
+  authorization    = "NONE"
+  http_method      = "POST"
+  api_key_required = true
+  rest_api_id      = aws_api_gateway_rest_api.mint_api.id
+  resource_id      = aws_api_getway_resource.mint_key_resource.id
+}
 
-# # resource "aws_api_gateway_resource" "create_nfid_resource" {
-# #   path_part   = "create_nfid"
-# #   parent_id   = aws_api_gateway_rest_api.nfid_api.root_resource_id
-# #   rest_api_id = aws_api_gateway_rest_api.nfid_api.id
-# # }
+// MINT CORS Method
+resource "aws_api_gateway_method" "mint_cors_method" {
+  rest_api_id   = aws_api_gateway_rest_api.mint_api.id
+  resource_id   = aws_api_gateway_resource.create_mint_key_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
 
-# // ------------------------------------------------------------------------------
-# #                       ===============================
-# #                        --- API GATEWAY METHOD(s) ---
-# #                       ===============================
+#                       !!!!!!!!!!!!!!
+#                       --- ITEM 2 ---
+#                       !!!!!!!!!!!!!!
 
-# #                       !!!!!!!!!!!!!!
-# #                       --- ITEM 1 ---
-# #                       !!!!!!!!!!!!!!
-# // MINT Post Method
-# resource "aws_api_gateway_method" "mint_post_method" {
-#   authorization = "NONE"
-#   http_method = "POST"
-#   api_key_required = true
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
-#   resource_id = aws_api_getway_resource.mint_key_resource.id
-# }
+// POST METHOD
+resource "aws_api_gateway_integration" "create_mint_integration" {
+  http_method             = aws_api_gateway_method.mint_post_method.http_method
+  resource_id             = aws_api_gateway_resource.create_mint_key_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.mint_api.id
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.requet_mint_key_lambda.invoke_arn
+}
 
-# // MINT CORS Method
-# resource "aws_api_gateway_method" "mint_cors_method" {
-#   rest_api_id   = aws_api_gateway_rest_api.mint_api.id
-#   resource_id   = aws_api_gateway_resource.create_mint_key_resource.id
-#   http_method   = "OPTIONS"
-#   authorization = "NONE"
-# }
+// CORS RESOURCE
+resource "aws_api_gateway_integration" "create_mint_cors_integration" {
+  http_method      = aws_api_gateway_method.mint_cors_method.http_method
+  resource_id      = aws_api_gateway_resource.create_mint_key_resource.id
+  rest_api_id      = aws_api_gateway_rest_api.mint_api.id
+  content_handling = "CONVERT_TO_TEXT"
 
-# #                       !!!!!!!!!!!!!!
-# #                       --- ITEM 2 ---
-# #                       !!!!!!!!!!!!!!
+  type = "MOCK"
 
-# // POST METHOD
-# resource "aws_api_gateway_integration" "create_mint_integration" {
-#   http_method = aws_api_gateway_method.mint_post_method.http_method
-#   resource_id = aws_api_gateway_resource.create_mint_key_resource.id
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
-#   type = "AWS_PROXY"
-#   integration_http_method = "POST"
-#   uri = aws_lambda_function.requet_mint_key_lambda.invoke_arn
-# }
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
 
-# // CORS RESOURCE
-# resource "aws_api_gateway_integration" "create_mint_cors_integration" {
-#   http_method = aws_api_gateway_method.mint_cors_method.http_method
-#   resource_id = aws_api_gateway_resource.create_mint_key_resource.id
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
-#   content_handling = "CONVERT_TO_TEXT"
+resource "aws_api_gateway_method_response" "mint_cors_method_response" {
+  http_method = aws_api_gateway_method.mint_cors_method.http_method
+  resource_id = aws_api_gateway_resource.create_mint_key_resource.id
+  rest_api_id = aws_api_gateway_rest_api.mint_api.id
+  status_code = 200
 
-#   type = "MOCK"
+  response_models = {
+    "application/json" = "Empty"
+  }
 
-#   request_templates = {
-#     "application/json" = "{ \"statusCode\": 200 }"
-#   }
-# }
+  depends_on = [
+    aws_api_gateway_method.mint_cors_method
+  ]
 
-# resource "aws_api_gateway_method_response" "mint_cors_method_response" {
-#   http_method = aws_api_gateway_method.mint_cors_method.http_method
-#   resource_id = aws_api_gateway_resource.create_mint_key_resource.id
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
-#   status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
 
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
+resource "aws_api_gateway_integration_response" "mint_cors_integration_response" {
+  http_method = aws_api_gateway_method.mint_cors_method.http_method
+  resource_id = aws_api_gateway_resource.create_mint_key_resource.id
+  rest_api_id = aws_api_gateway_rest_api.mint_api.id
+  status_code = 200
 
-#   depends_on = [
-#     aws_api_gateway_method.mint_cors_method
-#   ]
+  depends_on = [
+    aws_api_gateway_integration.create_mint_cors_integration,
+    aws_api_gateway_method_response.mint_cors_method_response
+  ]
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = true,
-#     "method.response.header.Access-Control-Allow-Methods" = true,
-#     "method.response.header.Access-Control-Allow-Origin"  = true
-#   }
-# }
-
-# resource "aws_api_gateway_integration_response" "mint_cors_integration_response" {
-#   http_method = aws_api_gateway_method.mint_cors_method.http_method
-#   resource_id = aws_api_gateway_resource.create_mint_key_resource.id
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
-#   status_code = 200
-
-#   depends_on = [
-#     aws_api_gateway_integration.create_mint_cors_integration,
-#     aws_api_gateway_method_response.mint_cors_method_response
-#   ]
-
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = "'Authorization, x-api-key, Content-Type'",
-#     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS, POST'",
-#     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-#   }
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Authorization, x-api-key, Content-Type'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS, POST'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
 
 
-# #                       !!!!!!!!!!!!!!
-# #                       --- ITEM 3 ---
-# #                       !!!!!!!!!!!!!!
+#                       !!!!!!!!!!!!!!
+#                       --- ITEM 3 ---
+#                       !!!!!!!!!!!!!!
 
-# resource "aws_api_gateway_deployment" "v1_mint_deployment" {
-#   rest_api_id = aws_api_gateway_rest_api.mint_api.id
+resource "aws_api_gateway_deployment" "v1_mint_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.mint_api.id
 
-#   triggers = {
-#     redeployment = sha1(jsonencode(timestamp()))
-#   }
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  triggers = {
+    redeployment = sha1(jsonencode(timestamp()))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# #                       !!!!!!!!!!!!!!
-# #                       --- ITEM 4 ---
-# #                       !!!!!!!!!!!!!!
+#                       !!!!!!!!!!!!!!
+#                       --- ITEM 4 ---
+#                       !!!!!!!!!!!!!!
 
-# resource "aws_api_gateway_stage" "v1_mint" {
-#   deployment_id = aws_api_gateway_deployment.v1_mint_deployment.id
-#   rest_api_id   = aws_api_gateway_rest_api.mint_api.id
-#   stage_name    = "V1_mint"
-# }
+resource "aws_api_gateway_stage" "v1_mint" {
+  deployment_id = aws_api_gateway_deployment.v1_mint_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.mint_api.id
+  stage_name    = "V1_mint"
+}
 
-# ## USAGE PLAN ITEMS
+## USAGE PLAN ITEMS
 
-# resource "aws_api_gateway_usage_plan" "mint_usage_plan" {
-#   name = "mint_usage_plan"
+resource "aws_api_gateway_usage_plan" "mint_usage_plan" {
+  name = "mint_usage_plan"
 
-#   api_stages {
-#     api_id = aws_api_gateway_rest_api.mint_api.id
-#     stage  = aws_api_gateway_stage.v1_mint.stage_name
-#   }
-# }
+  api_stages {
+    api_id = aws_api_gateway_rest_api.mint_api.id
+    stage  = aws_api_gateway_stage.v1_mint.stage_name
+  }
+}
 
-# resource "aws_api_gateway_api_key" "mint_api_key" {
-#   name = "mint_api_key"
-# }
+resource "aws_api_gateway_api_key" "mint_api_key" {
+  name = "mint_api_key"
+}
 
-# resource "aws_api_gateway_usage_plan_key" "mint_usage_plan_key" {
-#   key_id        = aws_api_gateway_api_key.mint_api_key.id
-#   key_type      = "API_KEY"
-#   usage_plan_id = aws_api_gateway_usage_plan.mint_usage_plan.id
-# }
+resource "aws_api_gateway_usage_plan_key" "mint_usage_plan_key" {
+  key_id        = aws_api_gateway_api_key.mint_api_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.mint_usage_plan.id
+}
 
-# // ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
