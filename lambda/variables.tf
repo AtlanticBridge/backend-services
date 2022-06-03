cc35f7b45c5625ff76dce62afd706ab5fba62cc9
Harry Shuttleworth <hshuttleworth0@gmail.com>
2022-06-03 16:02:29 +0100

    Add default policy and resource values

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,8 +1,12 @@
 variable "feature_name" {}
 
-variable "lambda_policy_actions" {}
+variable "lambda_policy_actions" {
+  default = ""
+}
 
-variable "lambda_policy_resources" {}
+variable "lambda_policy_resources" {
+  default = ""
+}
 
 variable "python_file_name" {}
 
