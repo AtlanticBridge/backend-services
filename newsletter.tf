f26a9d6227726918fc2dde3a477a245c43c590e0
Harry Shuttleworth <hshuttleworth0@gmail.com>
2022-06-04 13:00:12 +0100

    Test using brackets around multi line string

--- newsletter.tf
+++ newsletter.tf
@@ -1,22 +1,20 @@
 module "newsletter_lambda" {
-  source                  = "./lambda"
-  feature_name            = "newsletter"
-  lambda_policy_actions   = <<EOF
-  [
+  source       = "./lambda"
+  feature_name = "newsletter"
+  lambda_policy_actions = [<<EOF
     "dynamodb:PutItem",
     "logs:CreateLogGroup",
     "logs:CreateLogStream",
     "logs:PutLogEvents"
+  EOF 
   ]
-  EOF
-  lambda_policy_resources = <<EOF
-  [
+  lambda_policy_resources = [<<EOF
     module.nfid.nfid_users_table_arn,
     "arn:aws:logs:*:*:*"
-  ]
   EOF
-  python_file_name        = "newsletter_sign_up"
-  lambda_layers           = []
+  ]
+  python_file_name = "newsletter_sign_up"
+  lambda_layers    = []
   lambda_env_variables = {
     TABLE_NAME = "${module.nfid.nfid_users_table_id}"
   }
